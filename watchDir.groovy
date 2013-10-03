6105970e8a1aab9c4e3480366bc894458f9708f0
Pavel Alexeev (aka Pahan-Hubbitus) <pahan@hubbitus.info>
2013-10-03 20:45:52 +0400

    watchDir.groovy change dir

--- watchDir.groovy
+++ watchDir.groovy
@@ -1,15 +1,17 @@
+#!/bin/env groovy
+
 // https://gist.github.com/plecong/4343176
 
 import java.nio.file.FileSystems
 import java.nio.file.StandardWatchEventKinds
 
-def watchDir = new File('/home/pasha/temp/test.dir');
+def watchDir = new File('.');
 
 def watchService = FileSystems.getDefault().newWatchService();
 watchDir.toPath().register(watchService,
     StandardWatchEventKinds.ENTRY_MODIFY,
     StandardWatchEventKinds.ENTRY_CREATE)
- 
+
 println "Now watching for files at: ${watchDir.canonicalPath}"
 
 def i = 0;
@@ -18,7 +20,8 @@ while (true && i++ <= 100) {
  println "<$i>" + new Date()
     if (key) {
         key.pollEvents().each{
-            println new File(it.context().toString())
+//            println new File(it.context().toString())
+            println it.context().toString()
         }
         key.reset()
     }
